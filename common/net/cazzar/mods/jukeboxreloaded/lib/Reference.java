d89ceeeb5018c7f5d614151e327a70f0bc9c81a7
Cayde Dixon <cayde.dixon@gmail.com>
2013-07-20 22:10:39 +1000

    Build release.

--- common/net/cazzar/mods/jukeboxreloaded/lib/Reference.java
+++ common/net/cazzar/mods/jukeboxreloaded/lib/Reference.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2013 cazzar
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see [http://www.gnu.org/licenses/].
+ */
+
 package net.cazzar.mods.jukeboxreloaded.lib;
 
 import net.minecraft.util.ResourceLocation;
@@ -27,7 +44,7 @@ public class Reference {
     }
 
     public static final String MOD_ID = "JukeboxReloaded";
-    public static final String MOD_NAME = "JukeBox Reloaded";
+    public static final String MOD_NAME = "Jukebox Reloaded";
     public static final String MOD_VERSION = "@VERSION@";
     public static final String CHANNEL_NAME = "JUKEBOXRELOADED";
     public static final String SERVER_PROXY_CLASS = "net.cazzar.mods.jukeboxreloaded.proxy.CommonProxy";
