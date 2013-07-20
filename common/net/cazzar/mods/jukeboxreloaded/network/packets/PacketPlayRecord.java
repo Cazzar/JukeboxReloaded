517ee50cd1f9c3645e245db8d8d31c0833139d37
Cayde Dixon <cayde.dixon@gmail.com>
2013-07-20 18:01:33 +1000

    Dont need the WorldObj in SoundSystemHelper.playRecord and updates to the build.xml

--- common/net/cazzar/mods/jukeboxreloaded/network/packets/PacketPlayRecord.java
+++ common/net/cazzar/mods/jukeboxreloaded/network/packets/PacketPlayRecord.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2013 cazzar
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see [http://www.gnu.org/licenses/].
+ */
+
 package net.cazzar.mods.jukeboxreloaded.network.packets;
 
 import com.google.common.io.ByteArrayDataInput;
@@ -35,7 +52,7 @@ public class PacketPlayRecord extends PacketJukebox {
         if (te instanceof TileJukebox) {
             TileJukebox t = ((TileJukebox) te);
             t.setPlaying(true);
-            SoundSystemHelper.playRecord(record, t.worldObj, x, y, z, t.volume, t.getIdentifier());
+            SoundSystemHelper.playRecord(record, x, y, z, t.volume, t.getIdentifier());
         }
     }
 
