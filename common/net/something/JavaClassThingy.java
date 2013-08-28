47643936cccd947832aea0df33ff9ac0add2b48f
Cayde Dixon <cayde.dixon@gmail.com>
2013-08-28 18:50:54 +1000

    Added assets

---
+++ common/net/something/JavaClassThingy.java
@@ -0,0 +1,14 @@
+package net.something;
+
+public class JavaClassThingy {
+
+    private String Iloverefactoring;
+
+    public JavaClassThingy() {
+        omethingElse(null);
+    }
+
+    private void omethingElse(String that) {
+        String seomting = Iloverefactoring + that;
+    }
+}
